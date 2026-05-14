b1b2e9152a9a621aac1e3fa586f9ddb5a61ce3c1
Leonardo Gazola Medeiros <leonardo.gazolamedeiros@gmail.com>
2026-05-14 01:15:38 -0300

    height based shader texture

--- Assets/Scripts/Data/TextureData.cs
+++ Assets/Scripts/Data/TextureData.cs
@@ -1,17 +1,30 @@
 using UnityEngine;
 
-
 [CreateAssetMenu()]
-public class TextureData : UpdatableData
-{
-    public void ApplyToMaterial(Material material)
-    {
-        
-    }
+public class TextureData : UpdatableData {
 
-    public void UpdateMeshHeights(Material material, float minHeight, float maxHeight)
-    {
-        material.SetFloat("_MinHeight", minHeight);
-        material.SetFloat("_MaxHeight", maxHeight);
-    }
+	public Color[] baseColours;
+	[Range(0,1)]
+	public float[] baseStartHeights;
+
+	float savedMinHeight;
+	float savedMaxHeight;
+
+	public void ApplyToMaterial(Material material) {
+
+		material.SetInt ("baseColourCount", baseColours.Length);
+		material.SetColorArray ("baseColours", baseColours);
+		material.SetFloatArray ("baseStartHeights", baseStartHeights);
+
+		UpdateMeshHeights (material, savedMinHeight, savedMaxHeight);
+	}
+
+	public void UpdateMeshHeights(Material material, float minHeight, float maxHeight) {
+		savedMinHeight = minHeight;
+		savedMaxHeight = maxHeight;
+
+		material.SetFloat ("_MinHeight", minHeight);
+        material.SetFloat ("_MaxHeight", maxHeight);
+	}
+
 }
